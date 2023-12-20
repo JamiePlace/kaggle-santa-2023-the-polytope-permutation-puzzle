952867e3042693b821a97b2c47ff6d21723b8ac5
Jamie Place <jamieplace95@gmail.com>
2023-12-20 04:28:18 +0000

    nonsense

---
+++ src/eda.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import polars as pl
+from rich import print
+
+
+def load_info():
+    data = pl.scan_csv(
+        "data/puzzle_info.csv",
+    )
+    return data
+
+
+def load_data():
+    data = pl.read_csv(
+        "data/puzzles.csv",
+    )
+    return data
+
+
+df = load_data()
+print(df.head(5))
+print(df.shape)
+print(len(df["solution_state"][0]))
+print(len(df["initial_state"][0]))
