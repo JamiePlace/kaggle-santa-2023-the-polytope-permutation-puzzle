e1e44af49c4bab352b1aab63f965cd39c32b2cb6
Jamie Place <jamieplace95@gmail.com>
2023-12-21 02:56:13 +0000

    added scoring

--- run/train.py
+++ run/train.py
@@ -1,110 +1,34 @@
 import logging
 from pathlib import Path
-import os
-
 import hydra
-import torch
-from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.callbacks import (
-    LearningRateMonitor,
-    ModelCheckpoint,
-    RichModelSummary,
-    RichProgressBar,
-    EarlyStopping,
-)
-from pytorch_lightning.loggers.wandb import WandbLogger
+import pandas as pd
 
-# examples of imports for training
-# this is obviously to be updated depending on the project
 from src.conf import TrainConfig
-from src.modelmodule.somemodel import SomeModel
-from src.datamodule.somemodel import SomeModelDataModule
+from src.metric import score
 
-# performance due to my class gpu
-torch.set_float32_matmul_precision("medium")
+LOGGER = logging.getLogger(Path(__file__).name)
+PROJECT_NAME = "Santa-2023"
 
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s:%(name)s - %(message)s",
 )
-LOGGER = logging.getLogger(Path(__file__).name)
-PROJECT_NAME = "some_project_name"
 
 
 @hydra.main(config_path="conf", config_name="train", version_base="1.2")
 def main(cfg: TrainConfig):
-    # pre-process data to enable efficient data loading
+    LOGGER.info(f"Project name: {PROJECT_NAME}")
+    submission = pd.read_csv(Path(cfg.dir.sub_dir) / "submission.csv")
+    solution = pd.read_csv(Path(cfg.dir.data_dir) / "puzzles.csv")
 
-    seed_everything(cfg.seed)
-
-    LOGGER.info("Set Up DataModule")
-    datamodule = SomeModelDataModule(cfg)
-    # init lightning model
-    model = SomeModel(cfg, len(cfg.features), 3)
-
-    # set callbacks
-    checkpoint_cb = ModelCheckpoint(
-        verbose=True,
-        monitor=cfg.trainer.monitor,
-        mode=cfg.trainer.monitor_mode,
-        save_top_k=1,
-        save_last=False,
+    score_val = score(
+        solution,
+        submission,
+        series_id_column_name="id",
+        moves_column_name="moves",
+        puzzle_info_path=Path(cfg.dir.data_dir) / "puzzle_info.csv",
     )
-    lr_monitor = LearningRateMonitor("epoch")
-    early_stop_callback = EarlyStopping(
-        monitor=cfg.trainer.monitor,
-        min_delta=0.00,
-        patience=cfg.trainer.early_stopping_patience,
-        verbose=False,
-        mode=cfg.trainer.monitor_mode,
-    )
-    progress_bar = RichProgressBar()
-    model_summary = RichModelSummary(max_depth=2)
-
-    # init experiment logger
-    pl_logger = WandbLogger(
-        name=cfg.exp_name,
-        project=PROJECT_NAME,
-    )
-
-    pl_logger.log_hyperparams(cfg)  # type: ignore
-    trainer = Trainer(
-        # env
-        default_root_dir=Path.cwd(),
-        # num_nodes=cfg.training.num_gpus,
-        accelerator=cfg.trainer.accelerator,
-        precision="32",
-        # training
-        fast_dev_run=cfg.trainer.debug,  # run only 1 train batch and 1 val batch
-        max_epochs=cfg.trainer.epochs,
-        max_steps=cfg.trainer.epochs * len(datamodule.train_dataloader()),
-        gradient_clip_val=cfg.trainer.gradient_clip_val,
-        accumulate_grad_batches=cfg.trainer.accumulate_grad_batches,
-        callbacks=[
-            checkpoint_cb,
-            lr_monitor,
-            progress_bar,
-            model_summary,
-        ],
-        logger=pl_logger,
-        # resume_from_checkpoint=resume_from,
-        num_sanity_val_steps=0,
-        log_every_n_steps=int(len(datamodule.train_dataloader()) * 0.1),
-        sync_batchnorm=True,
-        check_val_every_n_epoch=cfg.trainer.check_val_every_n_epoch,
-    )
-    trainer.fit(model, datamodule=datamodule)
-
-    # load best weights
-    model = model.load_from_checkpoint(
-        checkpoint_cb.best_model_path,
-        cfg=cfg,
-        feature_dim=len(cfg.features),
-        num_classes=2,
-    )
-    weights_path = str("model_weights.pth")  # type: ignore
-    LOGGER.info(f"Extracting and saving best weights: {weights_path}")
-    torch.save(model.model.state_dict(), weights_path)
+    LOGGER.info(f"Score: {score_val}")
 
     return
 
--- src/conf.py
+++ src/conf.py
@@ -3,31 +3,14 @@ from dataclasses import dataclass
 
 
 @dataclass
-class DataClass1:
-    key1: int
-    key2: float
-    key3: bool
-
-
-@dataclass
-class DataClass2:
-    key4: list[int]
-    dc: DataClass1
-
-
-@dataclass
-class DataConfig:
-    batch_size: int
-    window_size: int
+class DirConfig:
+    data_dir: str
+    processed_dir: str
+    output_dir: str
+    model_dir: str
+    sub_dir: str
 
 
 @dataclass
 class TrainConfig:
-    learning_rate: float
-    dataconf: DataConfig
-
-
-@dataclass
-class InferenceConfig:
-    learning_rate: float
-    dataconf: DataConfig
+    dir: DirConfig
