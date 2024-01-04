ca4ab6ed3ed3c305a4f5259a79e4affd350cc695
Jamie Place <jamieplace95@gmail.com>
2024-01-04 16:08:15 +0000

    made resutlsDTO a bit more usable

--- src/dtos/ResultsDTO.py
+++ src/dtos/ResultsDTO.py
@@ -19,6 +19,28 @@ class ResultsDTO:
         self.combined_score = 0.0
         self.results = []
         self.cumulative_time = datetime.timedelta(0)
+        self.pos = -1
+
+    def __len__(self):
+        return len(self.results)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.pos += 1
+        if self.pos < len(self.results):
+            return self.results[self.pos]
+        raise StopIteration
+
+    def __getitem__(self, key):
+        return self.results[key]
+
+    def __str__(self):
+        output = ""
+        for result in self.results:
+            output += f"Puzzle ID: {result.puzzle_id}, Solved: {result.solved}, Errors: {result.error_count}, Score: {result.score}\n"
+        return output
 
     def add_result(self, resultDTO: ResultDTO):
         if resultDTO is not None:
