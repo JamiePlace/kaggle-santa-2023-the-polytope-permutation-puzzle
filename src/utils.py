22de5ddcebda45cb23b075c9dc939dcc199eedda
Jamie Place <jamieplace95@gmail.com>
2024-01-06 01:58:44 +0000

    start of investigations

--- src/utils.py
+++ src/utils.py
@@ -40,7 +40,7 @@ def cancel_pairs(moves:str) -> str:
     if len(pair)>0:
         head = pair.pop()
         # check if previous move is different to the last move of the group
-        if head.replace('-','') != group[-1].replace('-',''):
+        if len(group)==0 or head.replace('-','') != group[-1].replace('-',''):
             result.extend(group)
             group=deque([head])
         else:
@@ -48,6 +48,58 @@ def cancel_pairs(moves:str) -> str:
             inverse_head = '-'+head if positive_head else head
             if inverse_head in group:
                 group.remove(inverse_head)
+            else:
+                group.append(head)
+                
+    if len(group)>0:
+        result.extend(group)
+        
+    return '.'.join(result)
+def cancel_cube_pairs(moves):
+    moves = moves+'.'
+    pair = deque()
+    group = deque()
+    result = deque() 
+    move = ''
+    for i in moves:
+        if i != '.':
+            move += i
+        else:
+            if len(pair)<1:
+                pair.append(move)
+                move = ''
+                continue
+    
+            head = pair.pop()
+            
+            if head[1:] == move or move[1:]==head:
+                pass
+            else:
+                pair.append(move)
+                if len(group)<1:
+                    group.append(head)
+                else:
+                    if head.replace('-','')[0] != group[-1].replace('-','')[0]:
+                        result.extend(group)
+                        group=deque([head])
+                    else:
+                        move_opp = '-'+head if len(head)==2 else head[1:]
+                        if move_opp in group:
+                            group.remove(move_opp)
+                        else:
+                            group.append(head)
+            move=''
+     
+    # Collect remainder
+    if len(pair)>0:
+        head = pair.pop()
+        if head.replace('-','')[0] != group[-1].replace('-','')[0]:
+            result.extend(group)
+            group=deque([head])
+        else:
+            move_opp = '-'+head if len(head)==2 else head[1:]
+            if move_opp in group:
+                group.remove(move_opp)
             else:
                 group.append(head)
                 
